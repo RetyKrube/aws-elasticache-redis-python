e447433f3c506a847dd9a7d6b01dc2b2ca5b1968
RetyKrube <111463405+RetyKrube@users.noreply.github.com>
2022-09-14 17:26:12 -0400

    Update elasticache.py

--- elasticache.py
+++ elasticache.py
@@ -41,6 +41,13 @@ def write_redis_object(r, new_key, new_object):
     if temp_success:
         print('Successfully wrote object to Redis')
         
+# Read object from Redis cluster
+def read_redis_object(r, new_key):
+    temp_success = r.hgetall(new_key)
+    if temp_success:
+        print('Value of ' + new_key + ' :')
+        print(temp_success)
+        
 # Main program
 def main():
     redis_connection = config_redis_connection()
@@ -52,6 +59,7 @@ def main():
         'info3': 'This is info 3'
     }
     write_redis_object(redis_connection, 'myInfo', temp_obj)
+    read_redis_object(redis_connection, 'myInfo')
     
 if __name__ == '__main__':
     main()
